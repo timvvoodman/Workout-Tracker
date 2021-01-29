1b155d19f7d2ed34d2a4056575800c3bfb455a98
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-01-29 08:34:02 -0500

    html routes created and working on local host

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
+//Dependencies//
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+//Create Port
 const PORT = process.env.PORT || 3000;
 
+//link to mongoDB models
 //const db = require("./models");
 
+//initialize express server
 const app = express();
 
+//Data parsing w/Express
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {
+//Connect to Database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbname", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+//ROUTER//
+
+//html//
+require("./routes/htmlRoutes")(app);
+
+//api//
+require("./routes/apiRoutes")(app);
 
 ///Add exercises to the most recent workout plan.
 
