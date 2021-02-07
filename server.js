905b34060c73b170028afd0399893dd285907f80
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-02-07 14:17:04 -0500

    env variables for db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 //Create Port
 const PORT = process.env.PORT || 3000;
