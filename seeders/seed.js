5d6ac6c550c00f4d88a8690afb528f1359e933e0
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-02-09 22:11:49 -0500

    seed database

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,6 +1,6 @@
 let mongoose = require("mongoose");
 let db = require("../models");
-console.log(process.env.MONGODB_URI, "==============");
+console.log(process.env.MONGODB_URI, "MONGOURI==============");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
